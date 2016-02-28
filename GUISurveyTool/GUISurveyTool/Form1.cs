10228fd26541129e441e36706710652e7347cd10
Max Horowitz-Gelb <mhorowitzgelb@hotmail.com>
2016-02-27 21:40:09 -0600

    Started piecing together the GUI, audio recorder and voice recognition

---
+++ GUISurveyTool/GUISurveyTool/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+using NAudio.Wave;
+
+namespace GUISurveyTool
+{
+    public partial class Form1 : Form
+    {
+        private bool recording = false;
+        private WaveIn recorder;
+        private BufferedWaveProvider bufferedWaveProvider;
+        private SavingWaveProvider savingWaveProvider;
+        private WaveOut player;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!recording)
+            {
+                if(player != null)
+                {
+                    player.Stop();
+                }
+                recording = true;
+
+                button2.Visible = false;
+                button1.Text = "Stop Recording";
+
+                recorder = new WaveIn();
+                recorder.DataAvailable += RecorderOnDataAvailable;
+                bufferedWaveProvider = new BufferedWaveProvider(recorder.WaveFormat);
+
+                recorder.StartRecording();
+
+            }
+            else
+            {
+                recorder.StopRecording();
+                player = new WaveOut();
+                player.Init(bufferedWaveProvider);
+
+                button2.Visible = true;
+                recording = false;
+                button1.Text = "Start Recording";
+            }
+            
+        }
+
+        private void RecorderOnDataAvailable(object sender, WaveInEventArgs waveInEventArgs)
+        {
+            bufferedWaveProvider.AddSamples(waveInEventArgs.Buffer, 0, 
+                waveInEventArgs.BytesRecorded);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            player.Play();
+        }
+    }
+}
